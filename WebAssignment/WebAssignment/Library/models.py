89c5abe4e4cf7590ff13a910487b31486be081af
Omer Yassir <omerym10@gmail.com>
2024-05-23 17:59:27 +0300

    fdgfdgfdh

--- WebAssignment/WebAssignment/Library/models.py
+++ WebAssignment/WebAssignment/Library/models.py
@@ -6,6 +6,6 @@ class Book(models.Model):
     bookId = models.CharField(max_length=50, primary_key=True)
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
-    catogery = models.CharField(max_length=50, null=True)
+    category = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=255, null=True)
     isBorrowed = models.BooleanField(default=False)
