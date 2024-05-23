a16dd47c7dff6814f1ac1eb9e74c0e199efce777
Op-Bshaar <basharabdalla5@gmail.com>
2024-05-23 18:00:31 +0300

    .

--- WebAssignment/WebAssignment/Library/models.py
+++ WebAssignment/WebAssignment/Library/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    bookId = models.CharField(max_length=50, primary_key=True)
-    title = models.CharField(max_length=50)
+    bookId = models.CharField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=300)
     author = models.CharField(max_length=50)
     catogery = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=255, null=True)
